ec6463cce4efa9b6bfaac24721fcb23cec600974
Tamil Selvan <mselvan@agilimed.com>
2026-02-14 13:07:37 +0530

    changes done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("CI/CD Pipeline is working 🚀");
+  res.send("CI/CD Pipeline is working fine 🚀");
 });
 
 const PORT = process.env.PORT || 3000;
